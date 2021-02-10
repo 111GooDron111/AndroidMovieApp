b554bb923f583b09765a2f5bd405e89ded299859
Dmitry Abramovskikh <dmabram15@gmail.com>
2021-02-10 21:12:06 +0300

    initial commit

---
+++ app/src/main/java/com/dmabram15/androidmovieapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.dmabram15.androidmovieapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dmabram15.androidmovieapp.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/dmabram15/androidmovieapp/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.dmabram15.androidmovieapp.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
