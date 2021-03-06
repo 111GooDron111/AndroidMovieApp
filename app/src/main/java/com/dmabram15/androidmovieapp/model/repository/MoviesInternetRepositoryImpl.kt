be140e2bcd76e66e92dafc9cd2b93eb2e8c36db4
Dmitry Abramovskikh <dmabram15@gmail.com>
2021-03-06 17:54:42 +0300

    Добавил несколько recyclerView

--- app/src/main/java/com/dmabram15/androidmovieapp/model/repository/MoviesInternetRepositoryImpl.kt
+++ app/src/main/java/com/dmabram15/androidmovieapp/model/repository/MoviesInternetRepositoryImpl.kt
@@ -1,24 +1,18 @@
 package com.dmabram15.androidmovieapp.model.repository
 
 import com.dmabram15.androidmovieapp.api.MoviesAPI
-import com.dmabram15.androidmovieapp.model.Movie
 import com.dmabram15.androidmovieapp.model.MoviesDTO
 import com.dmabram15.androidmovieapp.view.TMDB_API_KEY
-import com.dmabram15.androidmovieapp.viewmodel.AppState
 import com.google.gson.GsonBuilder
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Callback
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.io.IOException
-import java.lang.Exception
-import java.lang.RuntimeException
-import java.time.Duration
 import java.util.concurrent.TimeUnit
 
-class InetMoviesRepo : MoviesInternetRepository {
+class MoviesInternetRepositoryImpl : MoviesInternetRepository {
 
     private val moviesService = Retrofit.Builder()
         .baseUrl("https://api.themoviedb.org/3/")
@@ -27,12 +21,22 @@ class InetMoviesRepo : MoviesInternetRepository {
                 GsonBuilder().setLenient().create()
             )
         )
-        .client(createOkHttpClient(WeatherApiInterceptor()))
+        .client(createOkHttpClient(MovieApiInterceptor()))
         .build()
         .create(MoviesAPI::class.java)
 
     override fun getMoviesForPeriod(period: String, callback: Callback<MoviesDTO>) {
-        moviesService.getRecommended(period, TMDB_API_KEY, "en-US").enqueue(callback)
+        moviesService.getTrending(period, TMDB_API_KEY, "en-US").enqueue(callback)
+    }
+
+    override fun getMoviesByGenres(isAdult: Boolean, genres: String, callback: Callback<MoviesDTO>) {
+        moviesService.getByGenres(
+            TMDB_API_KEY,
+            "en-US",
+            MoviesAPI.SORT_POPULARITY,
+            isAdult,
+            genres
+        ).enqueue(callback)
     }
 
     private fun createOkHttpClient(interceptor: Interceptor): OkHttpClient {
@@ -44,8 +48,7 @@ class InetMoviesRepo : MoviesInternetRepository {
         return httpClient.build()
     }
 
-
-    inner class WeatherApiInterceptor : Interceptor {
+    inner class MovieApiInterceptor : Interceptor {
         @Throws(IOException::class)
         override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
             return chain.proceed(chain.request())
